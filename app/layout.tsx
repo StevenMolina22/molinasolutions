2f8207d5f63a96ae385cefd4e770df29667716ce
Steven Molina <steven.molina.slayer@gmail.com>
2024-04-23 20:01:42 -0300

    deleted favicon link in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-      <link rel="shortcut icon" href="favicon.ico"/>
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
