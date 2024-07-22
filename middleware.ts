75413688d3e54300ff48f2ef33e8bc3b7a723009
steven <stevenmolina2205@gmail.com>
2024-07-21 23:56:18 -0300

    tries legacy middleware setting

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware, clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default authMiddleware({
+  publicRoutes: ["/"],
+})
+
+// export default clerkMiddleware();
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
